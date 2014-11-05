7c41bc26c7729da3d9f75176c670e47995f38d40
Sergi Almacellas Abellana <sergi@koolpi.com>
2014-11-05 17:25:11 +0100

    Migrate to version 3.4

--- tests/test_sale_opportunity_purchase_relation_margin.py
+++ tests/test_sale_opportunity_purchase_relation_margin.py
@@ -3,7 +3,8 @@
 import unittest
 import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_depends, doctest_dropdb
+from trytond.tests.test_tryton import test_depends
+from trytond.tests.test_tryton import doctest_setup, doctest_teardown
 
 
 class TestCase(unittest.TestCase):
@@ -23,6 +24,6 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
     suite.addTests(doctest.DocFileSuite(
             'scenario_sale_opportunity_purchase_relation_margin.rst',
-            setUp=doctest_dropdb, tearDown=doctest_dropdb, encoding='utf-8',
+            setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
